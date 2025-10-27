611bedde75e9eedf1ff443f8703ff5eab1d789e8
257072 <81724653+257072@users.noreply.github.com>
2025-10-27 21:55:15 +0000

    Add Human Amnesia poem to poem index

--- retrieval_layer.py
+++ retrieval_layer.py
@@ -89,3 +89,6 @@ with open('curated_poems/96_Heart_Supported_Mind_tokenised.json', 'r', encoding=
     heart_supported_mind_tokens = json.load(f)
 poem_index["Heart Supported Mind"] = heart_supported_mind_tokens
 
+with open('curated_poems/97_Human_Amnesia_tokenised.json', 'r', encoding='utf-8') as f:
+    human_amnesia_tokens = json.load(f)
+poem_index["Human Amnesia"] = human_amnesia_tokens
